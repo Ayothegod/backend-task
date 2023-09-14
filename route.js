a97e3f2cf4f16324daa896f60c450b77918ac1bd
Ayo <heyayomideadebisi@gmail.com>
2023-09-14 01:26:44 +0100

    omo

--- route.js
+++ route.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const {createPerson, getPerson, updatePerson, deletePerson} = require("./controller.js")
-
+// 
 router.get("/:user_id", getPerson
 )
 router.post("/", createPerson)
